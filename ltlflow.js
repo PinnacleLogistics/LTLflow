e07ad87dbac352ade024f838ca11ecffa35f01db
remrem-2022 <vallejo.rw@gmail.com>
2024-09-05 05:45:49 +0800

    updates

--- ltlflow.js
+++ ltlflow.js
@@ -210,7 +210,7 @@ async function submitForm(){
     formData.append('palletdet', JSON.stringify(palletdet));
     formData.append('pallet', JSON.stringify(pallet));
     formData.append('warehouse', "Bedtech");
-    formData.append('sendTo', "orders@bedtech.com");
+    formData.append('sendTo', "nick@pinnacleteam.com");
 
     for (let i = 0; i < pallet.length; i++) {
       if (pallet[i].pictureSidePalletDisplayfile != null ) {
